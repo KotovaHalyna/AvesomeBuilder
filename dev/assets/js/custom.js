b299bdb6bee1af79d8bc1c5fd1aff852dc187cb6
Halyna Kotova <kotovag,a777@gmail.com>
2024-06-26 19:09:22 +0300

    mouseover

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,17 @@
-  
+const buildItem = document.querySelectorAll('build-item')
+
+const cellAdress = document.querySelector('#address')
+const cellFlats = document.querySelector('#flats')
+const cellFlatsFree = document.querySelector('#flats-free')
+const cellFlatsBooking = document.querySelector('#flats-booking')
+const cellFlatsSold = document.querySelector('#flats-sold')
+
+console.log('log ', buildItem.length);
+
+buildItem.forEach(object => {
+    object.addEventListener('mouseover', ()=> {
+        const attr = object.getAttribute('data-address')
+        console.log(attr);
+        cellAdress.innerText = attr
+    })
+})
