a06f34f734434514a9d3a48ada1f029dc87e14c6
Halyna Kotova <kotovag,a777@gmail.com>
2024-07-01 19:28:39 +0300

    first

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,4 +1,4 @@
-const buildItem = document.querySelectorAll('build-item')
+const buildItem = document.querySelectorAll('.build-item')
 
 const cellAdress = document.querySelector('#address')
 const cellFlats = document.querySelector('#flats')
@@ -6,12 +6,48 @@ const cellFlatsFree = document.querySelector('#flats-free')
 const cellFlatsBooking = document.querySelector('#flats-booking')
 const cellFlatsSold = document.querySelector('#flats-sold')
 
-console.log('log ', buildItem.length);
 
-buildItem.forEach(object => {
+
+const showInformation =(cell, attr)=> buildItem.forEach(object => {
     object.addEventListener('mouseover', ()=> {
-        const attr = object.getAttribute('data-address')
-       
-        cellAdress.innerText = attr
+        const value = object.getAttribute(attr)
+        cell.innerText = value
     })
+
+   
 })
+
+showInformation(cellAdress, 'data-address')
+showInformation(cellFlats, 'data-flats')
+showInformation(cellFlatsFree, 'data-flats-free')
+showInformation(cellFlatsBooking, 'data-flats-booking')
+showInformation(cellFlatsSold, 'data-flats-sold')
+
+// const showNamber = (a, b, c, d)=> {
+//    console.log('Число: ', a, a, a, a, a, a, b, c, d, );
+// }
+
+
+// showNamber('96', 'arg-b', 'arg-c', 'arg-d Незалежності 15',)
+
+// const soldFlats = document.querySelectorAll('[data-flats-free="0"]')
+
+// soldFlats.forEach(item =>item.classList.add('sold'))
+
+const calcInformation = ()=> buildItem.forEach(item => {
+
+    let flats = Number(item.getAttribute('data-flats')); 
+    let flatsBooking = Number(item.getAttribute('data-flats-booking')) 
+    let flatsSold = Number(item.getAttribute('data-flats-sold')) 
+
+    let flatsFree = flats - (flatsBooking + flatsSold);
+
+    console.log(flatsFree);
+    
+    item.setAttribute('data-flats-free', flatsFree)
+
+    const dataFlatsFree = item.getAttribute('data-flats-free')
+    dataFlatsFree === "0" ? item.classList.add('sold') : undefined;
+})
+
+calcInformation()
